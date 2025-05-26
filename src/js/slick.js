5b3fe0bb2406330ac5c17c5e249d5260c4d6c1e8
いとしょん <itoshon.17@gmail.com>
2025-05-27 06:08:19 +0900

    スライダーなど修正

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,3 +1,5 @@
+console.log('現在の innerWidth:', window.innerWidth);
+
 $('.main__voice-container').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
@@ -10,7 +12,8 @@ $('.main__voice-container').slick({
       breakpoint: 768,
       settings: {
         slidesToShow: 1,    // SP は 1 枚
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        infinite: true,
       }
     }
   ]
